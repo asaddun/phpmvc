124081433a035d6a4a3d0f3af59f701c071eb8ac
Asad <muhammadasaddun@gmail.com>
2025-01-20 15:41:32 +0700

    pemisahan modal dari beberapa file jadi 1

--- app/views/ticket/history.php
+++ app/views/ticket/history.php
@@ -38,33 +38,5 @@
         </tbody>
     </table>
 </div>
-<!-- Modal -->
-<div class="modal fade" id="infoModal" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="infoModalLabel">Info Tiket</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="mb-3">
-                    <label for="name" class="form-label">Nama</label>
-                    <div class="form-control"><span id="fullname-info"></span></div>
-                </div>
-                <div class="mb-3">
-                    <label for="subject" class="form-label">Subjek</label>
-                    <div class="form-control"><span id="subject-info"></span></div>
-                </div>
-                <div class="mb-3">
-                    <label for="description" class="form-label">Deskripsi</label>
-                    <div class="form-control"><span id="description-info"></span></div>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-            </div>
-        </div>
-    </div>
-</div>
-<script>
-</script>
+
+<?php require_once '../app/views/ticket/modals_ticket.php' ?>
