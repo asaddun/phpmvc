757c06c59e92248242df56430e41e03700844ec8
Asad <muhammadasaddun@gmail.com>
2025-02-05 14:01:26 +0700

    menambahkan CDN AdminLTE

--- app/views/components/header.php
+++ app/views/components/header.php
@@ -7,6 +7,7 @@
     <title>Websiteku | <?php echo $data['judul'] ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?= BASEURL ?>/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script>
