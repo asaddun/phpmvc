de73a683e13f07d09ed741c80bdaa03f99c60d9b
Asad <muhammadasaddun@gmail.com>
2025-02-10 13:54:18 +0700

    perbaikan js untuk problem dan copy hasil version

--- public/js/script.js
+++ public/js/script.js
@@ -236,3 +236,61 @@ function status_ticket(status) {
       return "Deleted"; // user menghapus, batal membuat tiket
   }
 }
+
+function copyTable() {
+  let table = document.getElementById("dataTable");
+  let range = document.createRange();
+  range.selectNode(table);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();
+  alert("Data tabel telah disalin!");
+}
+
+const problemProcessModal = document.getElementById("problemProcessModal");
+if (problemProcessModal) {
+  problemProcessModal.addEventListener("show.bs.modal", function (event) {
+    const button = event.relatedTarget; // Tombol yang diklik
+    // console.log(button);
+    const subject_process = button.getAttribute("data-subject");
+    const description_process = button.getAttribute("data-description");
+    const status_process = button.getAttribute("data-status");
+
+    const processButton = document.getElementById("processButton");
+    const holdButton = document.getElementById("holdButton");
+    const closeButton = document.getElementById("closeButton");
+
+    const action_area = document.getElementById("action-process-area");
+    const action_textarea = document.getElementById("action-process-textarea");
+
+    // Isi modal
+    document.getElementById("subject-process").textContent = subject_process;
+    document.getElementById("description-process").value = description_process;
+    document.getElementById("status-process").textContent =
+      status_ticket(status_process);
+
+    if (status_process === "2") {
+      processButton.classList.remove("d-none");
+      holdButton.classList.add("d-none");
+      closeButton.classList.add("d-none");
+      action_area.classList.add("d-none");
+    } else if (status_process === "3") {
+      processButton.classList.add("d-none");
+      holdButton.classList.remove("d-none");
+      closeButton.classList.remove("d-none");
+      action_area.classList.remove("d-none");
+      action_textarea.disabled = false;
+      action_textarea.value = "";
+    } else if (status_process === "4") {
+      processButton.classList.remove("d-none");
+      holdButton.classList.add("d-none");
+      closeButton.classList.add("d-none");
+      action_area.classList.remove("d-none");
+      action_textarea.disabled = true;
+      if (action_process) {
+        action_textarea.value = action_process;
+      }
+    }
+  });
+}
