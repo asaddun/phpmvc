ae72ad9d6cc7012cd7e83bd247e0395d28f760cb
Asad <muhammadasaddun@gmail.com>
2025-05-26 16:10:34 +0700

    unduh semua package agar akses ke lokal resource

--- app/views/components/footer.php
+++ app/views/components/footer.php
@@ -3,9 +3,8 @@
     </main>
 <?php endif; ?>
 <?php Swal::swal(); ?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+<script src="<?= BASEURL ?>/bootstrap-5.3.6/js/bootstrap.bundle.min.js"></script>
+<script src="<?= BASEURL ?>/js/jquery-3.7.1.min.js"></script>
 <script src="<?= BASEURL ?>/js/script.js"></script>
 
 </body>
--- app/views/components/header.php
+++ app/views/components/header.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Websiteku | <?php echo $data['judul'] ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+    <link href="<?= BASEURL ?>/bootstrap-5.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= BASEURL ?>/fontawesome-6.7.2/css/all.min.css" rel="stylesheet">
     <link href="<?= BASEURL ?>/css/style.css" rel="stylesheet">
     <script>
         const BASEURL = "<?php echo BASEURL; ?>";
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="<?= BASEURL ?>/sweetalert2/dist/sweetalert2.all.min.js"></script>
 </head>
 
 <?php
