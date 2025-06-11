495918bea6e28b703e98f69e2e65183aa38e6cd3
Asad <muhammadasaddun@gmail.com>
2025-06-11 10:50:23 +0700

    update: ganti tampilan sidebar

--- app/views/components/header.php
+++ app/views/components/header.php
@@ -27,6 +27,49 @@ $url = explode('/', $url);
         <aside class="sidebar d-flex flex-column p-3 shadow">
             <a href="<?= BASEURL ?>" class="logo text-center mb-4"><i class="fa-solid fa-globe me-2"></i>Websiteku</a>
             <ul class="nav nav-pills flex-column mb-auto">
+
+                <!-- Ticket Menu -->
+                <div class="menu-title">Ticket</div>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($url[0] == "ticket" && $url[1] == "") ? "active" : "" ?>" href="<?= BASEURL ?>/ticket">
+                        <div><i class="fa-solid fa-ticket"></i> My Ticket</div>
+                    </a>
+                    <a class="nav-link <?= ($url[0] == "ticket" && $url[1] == "queue") ? "active" : "" ?>" href="<?= BASEURL ?>/ticket/queue">
+                        <div><i class="fa-solid fa-user-group"></i> Queue</div>
+                    </a>
+                    <a class="nav-link <?= ($url[0] == "ticket" && $url[1] == "history") ? "active" : "" ?>" href="<?= BASEURL ?>/ticket/history">
+                        <div><i class="fa-solid fa-calendar-day"></i> History</div>
+                    </a>
+                </li>
+
+                <!-- Service Menu -->
+                <div class="menu-title">Service</div>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($url[0] == "service" && $url[1] == "") ? "active" : "" ?>" href="<?= BASEURL ?>/service">
+                        <div><i class="fa-solid fa-calendar-day"></i> History</div>
+                    </a>
+                    <a class="nav-link <?= ($url[0] == "service" && $url[1] == "order") ? "active" : "" ?>" href="<?= BASEURL ?>/service/order">
+                        <div><i class="fa-solid fa-square-plus"></i> Order</div>
+                    </a>
+                    <a class="nav-link <?= ($url[0] == "service" && $url[1] == "price-list") ? "active" : "" ?>" href="<?= BASEURL ?>/service/price-list">
+                        <div><i class="fa-solid fa-money-bills"></i> Price List</div>
+                    </a>
+                </li>
+
+                <!-- Antrian Menu -->
+                <div class="menu-title">Antrian</div>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($url[0] == "queue" && $url[1] == "") ? "active" : "" ?>" href="<?= BASEURL ?>/queue">
+                        <div><i class="fa-solid fa-tv"></i> Display</div>
+                    </a>
+                    <a class="nav-link <?= ($url[0] == "queue" && $url[1] == "register") ? "active" : "" ?>" href="<?= BASEURL ?>/queue/register">
+                        <div><i class="fa-solid fa-square-plus"></i> Register</div>
+                    </a>
+                    <a href="<?= BASEURL ?>/queue/counter" class="nav-link <?= ($url[0] == "queue" && $url[1] == "counter") ? "active" : "" ?>">
+                        <div><i class="fa-solid fa-microphone"></i> Counter</div>
+                    </a>
+                </li>
+                <div class="menu-title">Other</div>
                 <li class="nav-item">
                     <a href="<?= BASEURL ?>/employee" class="nav-link <?= $url[0] == "employee" ? "active" : "" ?>">
                         <div><i class="fa-solid fa-user-group"></i> Employee</div>
@@ -37,91 +80,32 @@ $url = explode('/', $url);
                         <div><i class="fa-solid fa-list-check"></i> To Do List</div>
                     </a>
                 </li>
-
-                <!-- Ticket Menu -->
-                <li>
-                    <a class="nav-link <?= $url[0] == "ticket" ? "active" : "" ?>" data-bs-toggle="collapse" href="#submenu1" role="button" aria-expanded="false"
-                        aria-controls="submenu1">
-                        <span><i class="fa-solid fa-ticket"></i> Ticket</span>
-                        <i class="fa-solid fa-chevron-down submenu-icon"></i>
-                    </a>
-                    <div class="collapse submenu" id="submenu1">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link <?= ($url[0] == "ticket" && $url[1] == "") ? "active" : "" ?>" href="<?= BASEURL ?>/ticket"><i class="fa-solid fa-ticket"></i> My Ticket</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link <?= ($url[0] == "ticket" && $url[1] == "queue") ? "active" : "" ?>" href="<?= BASEURL ?>/ticket/queue"><i class="fa-solid fa-user-group"></i> Queue</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link <?= ($url[0] == "ticket" && $url[1] == "history") ? "active" : "" ?>" href="<?= BASEURL ?>/ticket/history"><i class="fa-solid fa-calendar-day"></i> History</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <!-- Service Menu -->
-                <li>
-                    <a class="nav-link <?= $url[0] == "service" ? "active" : "" ?>" data-bs-toggle="collapse" href="#submenu2" role="button" aria-expanded="false"
-                        aria-controls="submenu2">
-                        <span><i class="fa-solid fa-screwdriver-wrench"></i> Service</span>
-                        <i class="fa-solid fa-chevron-down submenu-icon"></i>
-                    </a>
-                    <div class="collapse submenu" id="submenu2">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link <?= ($url[0] == "service" && $url[1] == "") ? "active" : "" ?>" href="<?= BASEURL ?>/service"><i class="fa-solid fa-calendar-day"></i> History</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link <?= ($url[0] == "service" && $url[1] == "order") ? "active" : "" ?>" href="<?= BASEURL ?>/service/order"><i class="fa-solid fa-square-plus"></i> Order</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link <?= ($url[0] == "service" && $url[1] == "price-list") ? "active" : "" ?>" href="<?= BASEURL ?>/service/price-list"><i class="fa-solid fa-money-bills"></i> Price List</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <!-- Antrian Menu -->
-                <li>
-                    <a class="nav-link <?= $url[0] == "queue" ? "active" : "" ?>" data-bs-toggle="collapse" href="#submenu3" role="button" aria-expanded="false"
-                        aria-controls="submenu3">
-                        <span><i class="fa-solid fa-users-line"></i> Antrian</span>
-                        <i class="fa-solid fa-chevron-down submenu-icon"></i>
-                    </a>
-                    <div class="collapse submenu" id="submenu3">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link <?= ($url[0] == "queue" && $url[1] == "") ? "active" : "" ?>" href="<?= BASEURL ?>/queue"><i class="fa-solid fa-tv"></i> Display</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link <?= ($url[0] == "queue" && $url[1] == "register") ? "active" : "" ?>" href="<?= BASEURL ?>/queue/register"><i class="fa-solid fa-square-plus"></i> Register</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link <?= ($url[0] == "queue" && $url[1] == "counter") ? "active" : "" ?>" href="<?= BASEURL ?>/queue/counter"><i class="fa-solid fa-microphone"></i> Counter</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
+                <li class="nav-item">
                     <a href="<?= BASEURL ?>/meeting" class="nav-link <?= $url[0] == "meeting" ? "active" : "" ?>">
                         <div><i class="fa-solid fa-users"></i> Meeting</div>
                     </a>
                 </li>
-                <li>
+                <li class="nav-item">
                     <a href="<?= BASEURL ?>/book" class="nav-link <?= $url[0] == "book" ? "active" : "" ?>">
                         <div><i class="fa-solid fa-book"></i> Bookstore</div>
                     </a>
                 </li>
-                <li>
+                <li class="nav-item">
+                    <a href="<?= BASEURL ?>/locker" class="nav-link <?= $url[0] == "locker" ? "active" : "" ?>">
+                        <div><i class="fa-solid fa-door-closed"></i> Locker</div>
+                    </a>
+                </li>
+                <li class="nav-item">
                     <a href="<?= BASEURL ?>/version" class="nav-link <?= $url[0] == "version" ? "active" : "" ?>">
                         <div><i class="fa-solid fa-table-list"></i> Version</div>
                     </a>
                 </li>
-                <li>
+                <li class="nav-item">
                     <a href="<?= BASEURL ?>/settings" class="nav-link <?= $url[0] == "settings" ? "active" : "" ?>">
                         <div><i class="fa-solid fa-gear"></i> Settings</div>
                     </a>
                 </li>
-                <li>
+                <li class="nav-item">
                     <a href="<?= BASEURL ?>/auth/signout" class="nav-link">
                         <div><i class="fa-solid fa-right-from-bracket"></i> Logout</div>
                     </a>
